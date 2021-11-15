3c6685452c5ea18f7fbe7a0cf450f06e69ee3ed3
igorsanvaz <igorsanvaz@unifei.edu.br>
2021-11-15 14:55:50 -0300

    PROJETO 1

--- JS/codigo.js
+++ JS/codigo.js
@@ -9,55 +9,54 @@ ALUNOS: IGOR SANTOS VAZ - 2020022850
 */
 
 function quadrado() {
-  let lado = document.getElementById("ladoQuadrado").value
-  let area = Math.pow(lado, 2)
-  let apresentacao = document.getElementById("CalcQ")
-  apresentacao.innerHTML = area + " m²"
-  document.getElementById("ladoQuadrado").value = ""
-  return [area]
-  
+  let lado = document.getElementById("ladoQuadrado").value;
+  let area = Math.pow(lado, 2);
+  let apresentacao = document.getElementById("CalcQ");
+  apresentacao.innerHTML = area + " m²";
+  document.getElementById("ladoQuadrado").value = "";
+  return [area];
 }
 
 function circulo() {
-  let raio = document.getElementById("raioCirculo").value
+  let raio = document.getElementById("raioCirculo").value;
   let area = Math.PI * Math.pow(raio, 2);
-  let apresentacao = document.getElementById("CalcC")
-  apresentacao.innerHTML = area.toFixed(3) + " m²"
-  document.getElementById("raioCirculo").value = ""
-  return [area]
+  let apresentacao = document.getElementById("CalcC");
+  apresentacao.innerHTML = area.toFixed(3) + " m²";
+  document.getElementById("raioCirculo").value = "";
+  return [area];
 }
 
 function triangulo() {
-  let base = document.getElementById("ladoTriangulo").value
-  let altura = document.getElementById("altTriangulo").value
+  let base = document.getElementById("ladoTriangulo").value;
+  let altura = document.getElementById("altTriangulo").value;
   let area = (base * altura) / 2;
-  let apresentacao = document.getElementById("CalcT")
-  apresentacao.innerHTML = area + " m²"
-  document.getElementById("ladoTriangulo").value = ""
-  document.getElementById("altTriangulo").value = ""
-  return [area]
+  let apresentacao = document.getElementById("CalcT");
+  apresentacao.innerHTML = area + " m²";
+  document.getElementById("ladoTriangulo").value = "";
+  document.getElementById("altTriangulo").value = "";
+  return [area];
 }
 
 function retangulo() {
-  let base = document.getElementById("ladoRetangulo").value
-  let altura = document.getElementById("altRetangulo").value
+  let base = document.getElementById("ladoRetangulo").value;
+  let altura = document.getElementById("altRetangulo").value;
   let area = base * altura;
-  let apresentacao = document.getElementById("CalcR")
+  let apresentacao = document.getElementById("CalcR");
   apresentacao.innerHTML = area + " m²";
-  document.getElementById("ladoRetangulo").value = ""
-  document.getElementById("altRetangulo").value = ""
-  return [area]
+  document.getElementById("ladoRetangulo").value = "";
+  document.getElementById("altRetangulo").value = "";
+  return [area];
 }
 
 function trapezio() {
-  let baseMaior = document.getElementById("baseMaiorTrapezio").value
-  let baseMenor = document.getElementById("baseMenorTrapezio").value
-  let altura = document.getElementById("altTrapezio").value
+  let baseMaior = document.getElementById("baseMaiorTrapezio").value;
+  let baseMenor = document.getElementById("baseMenorTrapezio").value;
+  let altura = document.getElementById("altTrapezio").value;
   let area = ((parseInt(baseMaior) + parseInt(baseMenor)) * altura) / 2;
-  let apresentacao = document.getElementById("CalcTr")
-  apresentacao.innerHTML = area + " m²"
-  document.getElementById("baseMaiorTrapezio").value= ""
-  document.getElementById("baseMenorTrapezio").value = ""
-  document.getElementById("altTrapezio").value = ""
-  return [area]
+  let apresentacao = document.getElementById("CalcTr");
+  apresentacao.innerHTML = area + " m²";
+  document.getElementById("baseMaiorTrapezio").value = "";
+  document.getElementById("baseMenorTrapezio").value = "";
+  document.getElementById("altTrapezio").value = "";
+  return [area];
 }
